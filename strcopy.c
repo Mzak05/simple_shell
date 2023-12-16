f565fc3eb7a3907b303e4966c70189aaa7075dca
Ztote Achraf <achraf.ztote@gmail.com>
2023-12-15 18:40:15 -0800

    gates of shell

--- strcopy.c
+++ strcopy.c
@@ -1,11 +1,4 @@
 #include "shell.h"
-/**
- * _str_copy - copies a string
- * @desti: the destination
- * @sbuf: the source
- *
- * Return: pointer to destination
- */
 char *_str_copy(char *desti, char *sbuf)
 {
 	int i = 0;
@@ -21,12 +14,6 @@ char *_str_copy(char *desti, char *sbuf)
 	return (desti);
 }
 
-/**
- * _str_dup - duplicates a string
- * @dupstr: the string to duplicate
- *
- * Return: pointer to the duplicated string
- */
 char *_str_dup(const char *dupstr)
 {
 	int length = 0;
@@ -44,12 +31,6 @@ char *_str_dup(const char *dupstr)
 	return (ret);
 }
 
-/**
- *_puts - prints an input string
- *@prstr: the string to be printed
- *
- * Return: Nothing
- */
 void _puts(char *prstr)
 {
 	int i = 0;
@@ -63,13 +44,6 @@ void _puts(char *prstr)
 	}
 }
 
-/**
- * _putchar - writes the character c to stdout
- * @cp: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char cp)
 {
 	static int i;
